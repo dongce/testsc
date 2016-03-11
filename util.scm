74513687b0b5f489d13b0f50c4ee3b4570625c8b
dongil <dongce@gmail.com>
2016-03-11 16:55:26 +0900

    const value

--- util.scm
+++ util.scm
@@ -4,3 +4,12 @@
 
 (define-macro (mmsg . x)
   `(apply mmsg-set (quote ,x)))
+
+
+
+(define *PI* (* 2 (acos 0 )))
+(define *2PI* (* *PI* 2 ))
+(define (degree-to-radian degree) (* (/ degree 180.0)  *PI* ))
+(define (radian-to-degree radian) (/ (* radian 180.0) *PI*))
+(define (meter-to-dm meter) (/ meter 1828.8))
+(define (dm-to-meter dm)    (* dm 1828.8))
