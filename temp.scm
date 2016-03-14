5e2b006a7e309dd9a30963780ef69e7fceba0272
dongil <dongce@gmail.com>
2016-03-14 14:15:39 +0900

    track field support done

--- temp.scm
+++ temp.scm
@@ -1,4 +1,3 @@
 (testsc-init 1 )
 (testsc-track-nset 1 '(network_kinetics.x_position 10 ) '(transmission_info.AFRAME_1 12))
-(testsc-track-nset 1  '(transmission_info.AFRAME_1 12))
-(testsc-track-nget 1 'transmission_info.AFRAME_1 )
+(map (lambda (x) (testsc-track-nget 1 x)) '(transmission_info.AFRAME_1 network_kinetics.x_position) )
