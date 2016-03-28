d58d0b5a23c86fc5150adcf02c0cb1a762e49ad6
dongceldra <dongce@gmail.com>
2016-03-28 21:03:45 +0900

    const change

--- temp.scm
+++
@@ -1,4 +0,0 @@
-;; (testsc-init 1 )
-;; (testsc-track-nset 1 '(network_kinetics.x_position 10 ) '(transmission_info.AFRAME_1 12))
-;; (map (lambda (x) (testsc-track-nget 1 x)) '(transmission_info.AFRAME_1 network_kinetics.x_position) )
-
