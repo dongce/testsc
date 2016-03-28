cfcd2b60e5429de5a7e60809fc3a16d5273bc611
dongceldra <dongce@gmail.com>
2016-03-28 14:35:58 +0900

    xxx

--- javaut/javaut.java
+++ javaut/javaut.java
@@ -11,6 +11,7 @@ public class javaut {
 
    public static void main (String args[]) {
      try{
+       Boolean changed = false ; 
        System.out.println(args[0]) ; 
        FileInputStream fin = new FileInputStream(args[0]);
        ObjectInputStream ois = new ObjectInputStream(fin);
@@ -18,6 +19,7 @@ public class javaut {
        
        TestCodeStructure tcs = uttest.getTestCodeStructure() ;
        UtUserCode uuc = tcs.getGlobalUserCode() ;
+       UtUserCode ucs = tcs.getUserCodeStart() ;
 
        String usercode = uuc.getUserCode() ; 
        ois.close();
@@ -30,9 +32,19 @@ public class javaut {
                                      "void testsc_track_set(uint32_t id, network_track_data_ptr t ) ; " , 
                                      "void testsc_init(int testnum, const char *cmd, const char *homepath ) ; " , 
                                      "void testsc_eval(const char *cmd) ; ")) ;
-         // tcs.setGlobalUserCode(uuc) ; 
-         // uttest.setTestCodeStructure(tcs) ; 
+         changed = true ; 
+       }
 
+       usercode = ucs.getUserCode() ; 
+
+       if( "" == usercode.trim() ){
+         ucs.setUserCode(String.join("\r\n" ,
+                                     String.format("testsc_init(CS_TESTCASENO(), \"(testsc-require %s)\")", args[2])
+                                     )) ; 
+         changed = true ; 
+       }
+       
+       if(changed){
          ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(args[0]));
          oos.writeObject(uttest) ;
        }
