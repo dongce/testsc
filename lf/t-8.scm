0b556c1cae2986c3577abe006589f7f712855f26
dongceldra <dongce@gmail.com>
2016-04-01 00:22:20 +0900

    ticket:1 6b scm done

--- lf/t-8.scm
+++ lf/t-8.scm
@@ -9,6 +9,17 @@
    (testsc-track-nset 1 (list '_track_source_filter 1) )
    (testsc-track-nset 1 (list 'general.link_state  system_types-correlated) )
    )
+  
+  ((4)
+   (testsc-track-nset 1 (list '_track_source_filter 0) )
+   (testsc-track-nset 1 (list 'general.identity  system_types-hostile) )
+   )
+
+  
+  ((7)
+   (testsc-track-nset 1 (list '_track_source_filter 1) )
+   (testsc-track-nset 1 (list 'general.link_state  system_types-prohibit) )
+   )
   )
   
 
