5db8fb8f01c4bdb59ff5400f3510b00c8f080340
dongceldra <dongce@gmail.com>
2016-04-05 19:48:23 +0900

    data_bash_hash done

--- util.scm
+++ util.scm
@@ -106,7 +106,8 @@
   (if (= 1 (length args))
       (map list (car  args))
       (let ((cdrperm  (apply perm (cdr args))))
-        (car
+        (apply
+         append 
          (map
           (lambda (x)
             (map (lambda (y) (cons x y )) cdrperm)) (car args))))))
