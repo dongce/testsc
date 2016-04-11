2f6475b09857ed4900bfe311b6b93baf61cd2549
dongceldra <dongce@gmail.com>
2016-04-12 00:36:08 +0900

    dp_l11 event handler done

---
+++ DP_L11EventHandler/t-glofp.scm
@@ -0,0 +1,21 @@
+
+
+;; (testsc-require l11const)
+;; (testsc-require system_types)
+;; (testsc-require dlplink11types)
+
+(case (testsc-get-testnum)
+  ((9)
+   (define testsc 1 )
+   (for-each
+    (lambda (x)
+      (anset x)
+      )
+    
+    (num-list 0 30))
+
+
+   )
+   
+  )
+
