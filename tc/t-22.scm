c7ff53153fa019b0e89bef601ce747d03506abee
dongceldra <dongce@gmail.com>
2016-04-12 00:10:34 +0900

    track controller done

---
+++ tc/t-22.scm
@@ -0,0 +1,27 @@
+
+;; (testsc-require l11const)
+;; (testsc-require system_types)
+;; (testsc-require dlplink11types)
+
+(case (testsc-get-testnum)
+  ((39)
+   (define testsc 1 )
+   (define if 1 )
+   (define io 1 )
+   )
+
+  ((40)
+   (define testsc 1 )
+   (define if 1 )
+   (define io 0 )
+   )
+  ((41)
+   (define testsc 1 )
+   (define if 1 )
+   (define io 1 )
+   (define itd 1 )
+   )
+  
+  )
+
+
