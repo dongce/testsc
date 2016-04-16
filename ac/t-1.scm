2cbea77a064fdf49e84041871b7ecdd567f7b43e
dongceldra <dongce@gmail.com>
2016-04-16 20:44:44 +0900

    ticket:1 build_message_aircraft_done

--- ac/t-1.scm
+++ ac/t-1.scm
@@ -24,7 +24,14 @@
 
 
 
+
 (case (testsc-get-testnum)
+  ((8)
+   (set! testsc 1 )
+   (define mas 1)
+   (anset 0)
+
+   )
   ((10)
    (set! 1 )
    (set! order  system_types-launch_heli)
@@ -74,6 +81,16 @@
    (define itntn 1 )
    (tnset 0 )
    (tnset 1 )
+   )
+  ((17)
+   (set! testsc 1 )
+   (define itn 1 )
+   (define itntn 1 )
+   (define mas 1)
+   (set! order  system_types-cancel_handover)
+   (anset 0 '_aircraft_control.order system_types-assume_cntrl
+          '_aircraft_control.pu_transfer 1)
+   (tnset 0 )
    )
   
   )
