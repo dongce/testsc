8825384d83ce4339ff34375e8594dca92f94f11d
dongceldra <dongce@gmail.com>
2016-04-21 22:21:17 +0900

    compile done

---
+++ bm_ee/t-3.scm
@@ -0,0 +1,26 @@
+
+;; (testsc-require l11const)
+;; (testsc-require system_types)
+;; (testsc-require dlplink11types)
+
+(define testsc 1 )
+(define ittwn 1)
+(define ecounter 1)
+
+(case (testsc-get-testnum)
+  ((1)
+   (set! ittwn 0)
+
+   )
+
+  
+  ((4)
+   (set! ittwn 1)
+   (set! ecounter 1)
+   (anset 0)
+   (anset 1)
+   (anset 2)
+   )
+  )
+
+
---
+++ pc/t-2.scm
@@ -0,0 +1,21 @@
+
+;; (testsc-require l11const)
+(testsc-require system_types)
+;; (testsc-require dlplink11types)
+
+
+
+;; (testsc-require l11const)
+;; (testsc-require system_types)
+;; (testsc-require dlplink11types)
+
+(case (testsc-get-testnum)
+  ((18)
+   (define testsc 1 )
+   (tnset 0 'general.link_state -1)
+   )
+  )
+
+
+
+
