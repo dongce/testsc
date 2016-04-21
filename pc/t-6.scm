e44d851f29cdb74934674e01e876948df5a43d3e
dongceldra <dongce@gmail.com>
2016-04-21 21:34:50 +0900

    difference done

---
+++ pc/t-6.scm
@@ -0,0 +1,11 @@
+
+;; (testsc-require l11const)
+(testsc-require system_types)
+;; (testsc-require dlplink11types)
+
+
+
+
+(define testsc 1 )
+
+(tnset 0 'general.link_state system_types-transmitted_link_state)
