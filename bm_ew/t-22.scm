1b29ec3aa08ce677f069cc45fde6c379649e11a7
dongceldra <dongce@gmail.com>
2016-04-22 13:11:29 +0900

    ntds intf done

---
+++ bm_ew/t-22.scm
@@ -0,0 +1,15 @@
+(define testsc 1 )
+
+(define mas 5)
+
+(for-each (lambda ( x)  (anset x)) (num-list 0 5))
+
+
+
+(case (testsc-get-testnum)
+  ((1)
+   (define testsc 1 )
+   (define ewiv 1)
+   )
+  )
+
