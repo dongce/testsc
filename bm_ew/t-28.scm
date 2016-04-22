547af26bbdc1b0944ab0bda7cbdaec152dee4a10
dongceldra <dongce@gmail.com>
2016-04-22 15:56:46 +0900

    intf_vme pending

--- bm_ew/t-28.scm
+++ bm_ew/t-28.scm
@@ -2,6 +2,7 @@
 
 (define mas 1)
 (define ewor -1)
+(define ewpd 0)
 (anset 0 )
 (apply
  for-each-index-offset
@@ -10,3 +11,62 @@
    (set! ewor x))
  (num-list -1 10)
  )
+
+
+(for-each-index-offset
+ 10
+ (lambda (x)
+   (set! ewpd 2)
+   (set! ewor x))
+ 0
+ 0
+ 0
+ 3
+ 3
+ 4
+ 5
+ 4
+ )
+
+
+(case (testsc-get-testnum)
+  ((5)
+   (set! ewpd 2)
+   )
+
+
+  ((3)
+   (define ewct 1)
+   )
+  
+  ((11)
+   (define ewct 2)
+   )
+
+  ((12)
+   (define ewct 7)
+   (define vacec 1)
+   )
+  ((13)
+   (define ewct 7)
+   (define vacec 1)
+   )
+  ((14)
+   (define ewct 7)
+   (define vacec 0)
+   )
+  ((15)
+   (define ewct 2)
+   (define vacec 1)
+   )
+  ((16)
+   (define ewct 2)
+   (define vacec 1)
+   )
+  ((17)
+   (define ewct 1)
+   (define vacec 1)
+   )
+  
+  )
+
