1fbdcd9bfb18354c8c127494cc8adfd0c94b7a11
dongceldra <dongce@gmail.com>
2016-04-25 20:04:10 +0900

    ffxunit done

---
+++ bm_pairing_association/t-11.scm
@@ -0,0 +1,11 @@
+
+;; (testsc-require l11const)
+;; (testsc-require system_types)
+;; (testsc-require dlplink11types)
+
+(case (testsc-get-testnum)
+  ((7)
+   (define testsc 1 )
+   (anset 0)
+  ))
+
