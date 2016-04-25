1b7d759ab7c59ae68fcce6c101972bfac7ad6767
dongceldra <dongce@gmail.com>
2016-04-25 21:19:28 +0900

    pairing association done

---
+++ ti/t-1.scm
@@ -0,0 +1,12 @@
+
+;; (testsc-require l11const)
+;; (testsc-require system_types)
+;; (testsc-require dlplink11types)
+
+(case (testsc-get-testnum)
+  ((1)
+   (define testsc 1 )
+   (tnset 0 )
+   )
+  )
+
