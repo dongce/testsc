8f59e0e94dcf8888fb3ff8151df6da2a21dcbb73
dongil <dongce@gmail.com>
2017-05-29 09:03:19 +0900

    test script

---
+++ datamanager/t-dbhistoryreq.scm
@@ -0,0 +1,8 @@
+(define m_iDomain 1)
+(feio
+ 1
+ (lambda (x)
+   (set! m_iDomain x))
+
+ (num-list -2 10)
+ )
--- datamanager/t-send.scm
+++ datamanager/t-send.scm
@@ -1,14 +1,15 @@
 (testsc-debug "t-send called")
+
+(define i_iSize 10)
 (define i_enSendType 0)
 (for-each-index-offset
  1
  (lambda (x)
    (set! i_enSendType x))
-
-1 
-2 
-3 
-4 
-5 
-6  
+-1
+DSS_ADD        
+DSS_ADDSAME     
+DSS_UPDATE      
+DSS_UPDATEPART 
+100
  )
