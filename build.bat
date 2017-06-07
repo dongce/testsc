dcac8567b3a9b0cd50835bebf5e68f85e6fff5aa
dongil <dongce@gmail.com>
2017-06-08 08:45:03 +0900

    testsc struct

--- build.bat
+++ build.bat
@@ -1,24 +1,26 @@
 
 set PATH=C:\toolchain\gnutoolchain\gcc-4.7.2\bin;%PATH%
 
-rem scheme.exe 
-rem g++ -w -fpic -pedantic -I. -It:/gitdir/DLP_LINK11/src/csds_manager -It:/gitdir/DLP_LINK11/src/include -It:/gitdir/DLP_LINK11/src/common -It:/ICMS/devenv/icmsdlpcachelib/include -It:/ICMS/devenv/icmslib/include -c  -Wno-char-subscripts -O -DSTANDALONE=1 -DUSE_INTERFACE=1 -DUSE_STRLWR=0 -DUSE_STRCASECMP=0 -DUSE_DL=0 -DUSE_MATH=1 -DUSE_ASCII_NAMES=0 -DSTC_WIN32  t:/gitdir/DLP_LINK11/src/common/utility.cpp
+del scheme.exe *.o
 
-gcc -w -fpic -pedantic -I. -c  -Wno-char-subscripts -O -DSTANDALONE=1 -DUSE_INTERFACE=1 -DUSE_STRLWR=0 -DUSE_STRCASECMP=0 -DUSE_DL=0 -DUSE_MATH=1 -DUSE_ASCII_NAMES=0  scheme.c
+rem make scheme.exe 
+set CFLAGS=-w -fpic -pedantic -I. -c  -Wno-char-subscripts -O  -DUSE_INTERFACE=1 -DUSE_STRLWR=0 -DUSE_STRCASECMP=0 -DUSE_DL=0 -DUSE_MATH=1 -DUSE_ASCII_NAMES=0
 
-g++ -w -fpic -pedantic -I. -It:/gitdir/DLP_LINK11/src/include -It:/gitdir/DLP_LINK11/src/common -It:/ICMS/devenv/icmsdlpcachelib/include -It:\gitdir\DLP_LINK11\src\ntds_drv -It:\ICMS\devenv\icmslib\include -c  -Wno-char-subscripts -O -DSTANDALONE=1 -DUSE_INTERFACE=1  -DUSE_STRLWR=0 -DUSE_STRCASECMP=0 -DUSE_DL=0 -DUSE_MATH=1 -DUSE_ASCII_NAMES=0  testsc.cpp
+set EXTRAINCLUDE=-It:/gitdir/DLP_LINK11/src/include -It:/gitdir/DLP_LINK11/src/common -It:/ICMS/devenv/icmsdlpcachelib/include -It:\gitdir\DLP_LINK11\src\ntds_drv -It:\ICMS\devenv\icmslib\include
 
+gcc %CFLAGS% -DSTANDALONE=1 scheme.c
+
+g++ %CFLAGS% -DSTANDALONE=1 %EXTRAINCLUDE%  testsc.cpp 
 
 g++ -w -fpic -pedantic -o scheme  -Wno-char-subscripts  testsc.o scheme.o -lm
 
 
-rem libtinyscheme.a
+rem make libtestsc.a
 
-rem g++ -w -fpic -pedantic -I. -It:/gitdir/DLP_LINK11/src/csds_manager -It:/gitdir/DLP_LINK11/src/include -It:/gitdir/DLP_LINK11/src/common -It:/FFX-II/devenv/icmsdlpcachelib/include -It:/FFX-II/devenv/icmslib/include -c  -Wno-char-subscripts -O -DSTANDALONE=0 -DUSE_STRLWR=0 -DUSE_STRCASECMP=0 -DUSE_DL=0 -DUSE_MATH=1 -DUSE_ASCII_NAMES=0 -DSTC_WIN32  t:/gitdir/DLP_LINK11/src/common/utility.cpp
+del libtestsc.a *.o
 
-
-gcc -w -fpic -pedantic -I. -c  -Wno-char-subscripts -O  -DSTANDALONE=0 -DUSE_STRLWR=0 -DUSE_STRCASECMP=0 -DUSE_DL=0 -DUSE_MATH=1 -DUSE_ASCII_NAMES=0  scheme.c
-g++ -w -fpic -pedantic -I. -It:/gitdir/DLP_LINK11/src/include -It:/gitdir/DLP_LINK11/src/common -It:/ICMS/devenv/icmsdlpcachelib/include -It:\gitdir\DLP_LINK11\src\ntds_drv -It:\ICMS\devenv\icmslib\include -c  -Wno-char-subscripts -O -DSTANDALONE=0 -DUSE_INTERFACE=1  -DUSE_STRLWR=0 -DUSE_STRCASECMP=0 -DUSE_DL=0 -DUSE_MATH=1 -DUSE_ASCII_NAMES=0  testsc.cpp
+gcc %CFLAGS% -DSTANDALONE=0 scheme.c
+g++ %CFLAGS% -DSTANDALONE=0 %EXTRAINCLUDE%   testsc.cpp
 
 
 ar crs libtestsc.a scheme.o testsc.o 
