3249b880ff905d3af5793903459408e58999e2e3
dongil <dongce@gmail.com>
2017-06-09 23:11:35 +0900

    version done

--- home/ex/t-1.scm
+++ home/ex/t-1.scm
@@ -1,55 +1,30 @@
-(define   pri_surf_ampl_no_statement               0)
-(define   pri_surf_ampl_combatant                  1 )
-(define   pri_surf_ampl_line                       2 )
-(define   pri_surf_ampl_patrol_vessel              3 )
+;;; 상수 정의 - 공통파일을 만들어 테스트간에 공유할 수 있다. 
 (define   pri_surf_ampl_amphibious                 4 )
-(define   pri_surf_ampl_non_naval                  5 )
-(define   pri_surf_ampl_non_combatant              6 )
-(define   pri_surf_ampl_mine_warfare               7 )
-(define   pri_surf_ampl_size                       8 )
-
-(define   sec_surf_ampl_no_statement               0)
-(define   sec_surf_ampl_carrier                     1 )
-(define   sec_surf_ampl_destroyer                   2 )
-(define   sec_surf_ampl_asw                         3 )
-(define   sec_surf_ampl_asuw                        4 )
-(define   sec_surf_ampl_underway_replenishment      5 )
-(define   sec_surf_ampl_fleet_support               6 )
-(define   sec_surf_ampl_hospital                    7 )
-(define   sec_surf_ampl_mine_layer                  8 )
-(define   sec_surf_ampl_mine_sweeper                9 )
-(define   sec_surf_ampl_cruiser                    10 )
 (define   sec_surf_ampl_frigate                    11 )
 (define   sec_surf_ampl_landing_ship               12 )
-(define   sec_surf_ampl_landing_craft              13 )
-(define   sec_surf_ampl_assault_vessel             14 )
-(define   sec_surf_ampl_intelligence               15 )
-(define   sec_surf_ampl_merchant                   16 )
-(define   sec_surf_ampl_fishing                    17 )
-(define   sec_surf_ampl_leisure                    18 )
-(define   sec_surf_ampl_acv_hydro                  19 )
-(define   sec_surf_ampl_auxiliary                  20 )
-(define   sec_surf_ampl_battleship                 21 )
-(define   sec_surf_ampl_cargo                      22 )
-(define   sec_surf_ampl_command                    23 )
-(define   sec_surf_ampl_sec_surf_ampl_destroyer_aaw 24 )
-(define   sec_surf_ampl_sec_surf_ampl_destroyer_asw 25 )
-(define   sec_surf_ampl_landing_platform           26 )
-(define   sec_surf_ampl_ocean_research             27 )
-(define   sec_surf_ampl_surfaced_submarine         28 )
-(define   sec_surf_ampl_tanker                     29 )
-(define   sec_surf_ampl_size                       30 ) 
 
+
+;;; 테스트변수 
 (define priampl 0)
 (define secampl 0)
+
 (foreach-tnum
- 1
- (lambda (x)
-   (set! priampl (nth 0 x))
-   (set! secampl (nth 1 x)))
+ 1                                      ; testcase 시작값
+ (lambda (x)                            ; testcase 번호에서 testcase 시작값을 뺀 번째 조합 값을 파라미터로 전달한다. 
+   (set! priampl (nth 0 x))             ; 조합값의 첫번째 값은 priampl
+   (set! secampl (nth 1 x)))            ; 조함값의 두번째 값은 secampl
  (combi (num-list 0 8) (num-list 0 30)))
 
-(case (testsc-get-testnum)              
-  ((241)
-   (testsc-debug "241")
-   (set! priampl -1))) 
+;;; combi (num-list 0 8 ) (num-list 0 30 ) 
+;;; -> ((0 0 ) (0 1 ) (0 2 )... (0 29) (1 0 ) (1 1 ) ...)
+;;;     ↑ testcase 시작 값과 같은 경우 lambda 함수로 전달
+;;;            ↑ testcase 시작 값보다 1 클 경우 lambda 함수로 전달
+
+(case (testsc-get-testnum)              ; C 의 switch 문에 해당 - testcase 번호
+  ((241)                                ; C 의 case 문에 해당 - testcase 번호가 241 일 경우  
+   (set! secampl -1)                    ; 테스트변수 secampl 을 -1 로 설정한다.
+   (set! priampl -1))                   ; 테스트변수 priampl 을 -1 로 설정한다.
+  ((242 243)                            ; 테스트번호 나열이 가능하다. 
+   (set! priampl pri_surf_ampl_amphibious)      ; define 으로 정의된 상수값을 사용할 수 있다. 
+   (set! secampl sec_surf_ampl_landing_ship)))  ; define 으로 정의된 상수값을 사용할 수 있다. 
+
